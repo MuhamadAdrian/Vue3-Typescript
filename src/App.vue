0dddefa8a55cf68409746af2076a3dd664fd8be1
MuhamadAdrian <madrian.septiana@gmail.com>
2022-06-13 11:44:38 +0700

    finish training

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order by Title</button>
+        <button @click="handleClick('salary')">Order by Salary</button>
+        <button @click="handleClick('location')">Order by Location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import JobList from './components/JobList.vue';
+import Job from './types/Job';
+import OrderTerm from './types/OrderTerm';
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    JobList
+  },
+
+  setup(){
+    // const state = reactive<object>({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // });
+
+    // return {
+    //   ...toRefs(state)
+    // }
+
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ]);
+
+    const order = ref<OrderTerm>('title');
+
+    const handleClick = (term: OrderTerm) : void => {
+      order.value = term;
+
+      console.log(order.value);
+    }
+
+    return {
+      jobs, order, handleClick
+    }
   }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
